ad57768fb79b66803d485c423995f77ded598783
CristianReyes04 <cristianreyes04@hotmail.com>
2026-07-07 22:30:13 -0500

    Correct input prompt and fix typo in output function
    Fix typo in number_input function and correct wite to write.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ st.title("Proyecto módulo 1 Fundamentals")
 st.sidebar.title("Parámetros")
 
 valor_inicial = st.number_input("Ingrese el valor inicial: ", value=0)
-valor_final = st.number_input("Ingrese el valor Final: "), value=1
+valor_final = st.number_input("Ingrese el valor Final: ", value=1)
 
 lista_numerica = list(range(valor_inicial,valor_final))
 #similar a print es wite
