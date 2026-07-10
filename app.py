cc3b5f57a3b462855afc738cfb58ab75856c96d3
CristianReyes04 <cristianreyes04@hotmail.com>
2026-07-09 23:47:56 -0500

    Refactor output messages in app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ st.image("Logo_python.png")
 st.sidebar.image("Logo_inst.png")
 
 modulo = st.sidebar.selectbox("Elija un módulo", ["Módulo Listas","Módulo Array","Módulo funciones"])
-
 if modulo == "Módulo Listas":
   valor_inicial = st.number_input("Ingrese el valor inicial: ", value=0)
   valor_final = st.number_input("Ingrese el valor Final: ", value=1)
@@ -16,17 +15,13 @@ if modulo == "Módulo Listas":
   #similar a print es write
   st.write(lista_numerica)
 elif modulo == "Módulo Array":
-   st.write("Estas en el módulo de arreglos")
-  
+  st.write("Estas en el módulo de arreglos")
   limite_inferior = st.number_input("Ingrese el límite inferior", value=1200)
   limite_superior = st.number_input("Ingrese el límite superior", value = 1250)
   cantidad_datos =  st.number_input("Ingrese totalidad de datos a crear", value = 31)
-
   datos_produccion = np.random.randint(limite_inferior, limite_superior, cantidad_datos)
-
   st.write(datos_produccion)
-
   st.write("La producción total es:" ,  np.sum(datos_produccion))
   st.write("La producción promedio es:" , np.mean(datos_produccion) )
 else:
-   st.write("Estas en el módulo de funciones")
+  st.write("Estas en el módulo de funciones")
